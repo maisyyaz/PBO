a3a2d57cbb4f43c09c8da1eec3837e7a7671a133
maisyyaz <maisyyazid@student.telkomuniversity.ac.id>
2021-10-22 11:08:03 +0700

    tugas 5

--- src/com/yazid/tugas5/bagian1/Lingkaran.java
+++ src/com/yazid/tugas5/bagian1/Lingkaran.java
@@ -3,20 +3,23 @@ package com.yazid.tugas5.bagian1;
 public class Lingkaran {
     private final float PI = 3.14f;
     private final float jariJari;
+    private final double diameter;
 
     public Lingkaran(float jariJari) {
         this.jariJari = jariJari;
+        this.diameter = 2 * jariJari;
     }
 
     public Lingkaran(double diameter) {
+        this.diameter = diameter;
         this.jariJari = (float) (diameter / 2);
     }
 
     public double getLuas() {
-        return PI * Math.pow(jariJari, 2);
+        return PI * Math.pow(this.jariJari, 2);
     }
 
     public double getKeliling() {
-        return PI * 2 * jariJari;
+        return PI * this.diameter;
     }
 }
